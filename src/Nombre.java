e5e337ca613537a13e2a4c20ff8264dca17e59c2
angelarmandodz <arman@LAPTOP4D766JKI>
2023-04-23 22:31:27 -0600

    cambio1

---
+++ src/Nombre.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Nombre {
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("¿Como te llamas? ");
+	String respuesta = sc.next();
+	System.out.println(respuesta);
+	}
+}
